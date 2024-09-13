3c23ac3960b02bee4ede81398a36f47e68b8f0ba
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-09-14 00:33:55 +0100

    The simple interest update

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,9 +1,10 @@
-current year = 2023; current age = 30
+current year = 2023;
+user current age = 30
 future year = 2050
 
 years apart = future year - current year
 years apart = 2050 - 2023
 
-age = years apart + current age
+age = years  + user current age
 
- print(f"In 2050, you will be {age} years old")
+print(f"In 2050, you will be {age} years old")
