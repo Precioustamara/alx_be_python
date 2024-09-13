e93ed4bc0c01f9f4b017c0b2e3b05b769b295ffc
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-09-14 00:43:02 +0100

    age calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,10 +1,6 @@
-current year = 2023;
-user current age = 30
-future year = 2050
-
-years apart = future year - current year
-years apart = 2050 - 2023
-
-age = years  + user current age
+"How old are you?"
+2023 = 30
+2050 - 2023 = 27
+age = 30  +  27
 
 print(f"In 2050, you will be {age} years old")
