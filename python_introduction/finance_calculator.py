7b8b6a578a00ca1c40ca6f9d44ddee3f9e9cd585
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-09-14 23:21:42 +0100

    personal finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,5 +5,5 @@ monthly_savings = monthly_income - monthly_expenses
 
 projected_savings = monthly_savings * 12 + {monthly_savings * 12 * 0.05}
 
-print(f"monthly_savings")
-print(f"projected_savings")
+print(f"The user’s monthly savings is {monthly_savings}" )
+print(f"The projected annual savings after including interest is {projected_savings}" )
