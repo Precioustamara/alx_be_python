71642c25862bc62de359a1240df3ece85797036b
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-09-20 00:27:35 +0100

    Match Calculator Operations

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,35 @@
-operation = input("Enter the first number: "
-     "Enter the second number: "
-    "Choose the operation(+, -, *, /): "
-    "The result is ")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input ("Choose the operation(+, -, *, /): ")
+result = 0
 
-match operation:
- case addition:  
-  #print(operation) 
- #case subtract:
- # print("The result is ")
-#case multiply (*):
- # print("The result is ")
-#case divide(/):
- # print("The result is ")
-#case _:
- #   print("invalid operation")
+##match operation:
+   #case '+':
+    #result = num1 + num2
+    #print("The result is ")
+   #case '-':
+    #result = num1 - num2
 
-print("Enter the first number: ")
-print("Enter the second number: ")
-print("choose the operation(+, -, *, /): ")
-print("The result is ")
+match operator:
+    case '+':
+        result = num1 + num2
+        print(f"The result is {result}")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}")
+    case '*':
+        result = num1 * num2
+        print(f"The result is {result}")
+    case '/':
+        if num2 == 0:
+            print(f"Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}")
+    case _:
+        print(f"This operation is invalid")
+        
+
+
+
 
