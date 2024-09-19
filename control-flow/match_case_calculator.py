1ec28427edc73253c9e50a3ae8ee4a8e6d9a5157
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-09-19 23:14:28 +0100

    git mergr master to main

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+operation = input("Enter the first number: "
+     "Enter the second number: "
+    "Choose the operation(+, -, *, /): "
+    "The result is ")
+
+match operation:
+ case addition:  
+  #print(operation) 
+ #case subtract:
+ # print("The result is ")
+#case multiply (*):
+ # print("The result is ")
+#case divide(/):
+ # print("The result is ")
+#case _:
+ #   print("invalid operation")
+
+print("Enter the first number: ")
+print("Enter the second number: ")
+print("choose the operation(+, -, *, /): ")
+print("The result is ")
+
