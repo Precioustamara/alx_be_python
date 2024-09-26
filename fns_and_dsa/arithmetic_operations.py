9f2bb3fea5871938038d41e40105fcca4c5d7786
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-09-26 22:30:59 +0100

    arithmetic operations import

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num1 == 0:
+        if num2 == 0:
           return 'it cannot divide by zero'
         else:
           return num1 / num2
