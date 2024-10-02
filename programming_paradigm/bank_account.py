26a5750bbcfcc28eab373c0df73cb420db19539f
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-10-03 00:20:07 +0100

    Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,17 @@
 class BankAccount:
-    def __init__(self, account_balance = 0):
-        self.account_balance = account_balance
+    def __init__(self, initial_balance = 0):
+         self.account_balance = initial_balance
 
     def deposit(self, amount):
-        self.account_balance += amount
-        print(f"Deposited: ${amount}")
-        
+         self.account_balance += amount
+         print(f"Deposited: ${amount}")
+
     def withdraw(self, amount):
          if amount <= self.account_balance:
               self.account_balance -= amount
               print(f"Withdrew: ${amount}")
          else:
-              print(f"Insufficient funds.")
-        
+              print("Insufficient funds.")
+
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+         print(f"Current Balance: ${self.account_balance}")
