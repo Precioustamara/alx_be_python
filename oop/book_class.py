4600a662f15f3a26cc4a6f36339cbe24cb93ad68
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-10-11 00:36:38 +0100

    Book class output correction

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,11 +5,11 @@ class Book:
         self.year = year
 
     def __del__(self):
-        user = input(f"Do you want to delete the {self.title} ? (y/n): ").lower()
-        if user == 'y':
-            print(f"Deleting {self.title}")
-        else:
-            pass
+        #user = input(f"Do you want to delete the {self.title} ? (y/n): ").lower()
+        #if user == 'y':
+        print(f"Deleting {self.title}")
+        #else:
+         #   pass
         
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
